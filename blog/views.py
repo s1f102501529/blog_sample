bef2e138bd4536968c17771fe1cccc7e5ebd07cf
s1f102501529 <s1F102501529@iniad.org>
2025-12-03 23:19:53 +0900

    F

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import random
-
+fortune=random.randint(1,3)
 # Create your views here.
 from django.http import HttpResponse
 
@@ -9,23 +9,14 @@ def index(request):
 def update(request, article_id):
 	return HttpResponse("article_id: {}".format(article_id))
 def hello(request):
-    fortune=random.randit(1,3)
+    fortune
     isGreatFortune=False
     fortuneMessage=''
-    if fortune == 1:
-        fortuneMessage = 'Great Fortune'
-        isGreatFortune = True  # 大吉の場合のみ True にする
-    elif fortune == 2:
-        fortuneMessage = 'Small Fortune'
-        isGreatFortune = False
-    else:
-        fortuneMessage = 'Bad Fortune'
-        isGreatFortune = False
     data={
          'name':'Alice',
          'weather':'CLOUDY',
          'wether_detail':['Temperature:23℃','Humidity:40%','Wind: 5m/s'],
-         'isGreatFortune':isGreatFortune,
-         'fortune':'Great Fortune!'
+         'isGreatFortune': isGreatFortune,
+         'fortune': fortuneMessage
     }
     return render(request, 'blog/hello.html',data)
