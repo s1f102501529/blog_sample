d5f036e4276d493c88a540994c95dfb9cb724943
s1f102501529 <s1F102501529@iniad.org>
2025-12-03 18:17:10 +0900

    C

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+	return render(request, 'blog/index.html')
+def update(request, article_id):
+	return HttpResponse("article_id: {}".format(article_id))
+def hello(request):
+    return render(request, 'blog/hello.html')
