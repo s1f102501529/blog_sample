1360f47f45aadb15a004abd808d07489b564196b
s1f102501529 <s1F102501529@iniad.org>
2025-12-07 03:42:25 +0900

    H

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    posted_at = models.DateTimeField(default=timezone.now)
+    published_at = models.DateTimeField(blank=True, null=True)
+    like = models.IntegerField(default=0)
+
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
